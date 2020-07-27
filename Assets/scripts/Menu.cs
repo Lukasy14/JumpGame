d3cc25c25ed87e30144ccc6155a2d10a1ef27632
Lukasy14 <sumlngremote@gmail.com>
2020-07-27 11:35:41 +0800

    Pause menu function completed. Now you can pause the game and use the menu. You can reset the game in the Menu.

--- Assets/scripts/Menu.cs
+++ Assets/scripts/Menu.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject pauseMenu;
+    public AudioMixer audioMixer;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -18,5 +21,28 @@ public class Menu : MonoBehaviour
     public void UIEnable()
     {
         GameObject.Find("Canvas/MainMenu/UI").SetActive(true);
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void offPause()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void resteGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void SetVolume(float value)
+    {
+        audioMixer.SetFloat("MainVolume", value);
     }
 }
